d60b9d418e8c979a57bc4b760680541c70b2776c
jackyu104 <jack.yu@104.com.tw>
2020-08-12 15:12:28 +0800

    調整為switch判斷

--- php/src/GildedRoseRefact.php
+++ php/src/GildedRoseRefact.php
@@ -19,42 +19,53 @@ final class GildedRoseRefact
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-
-            if ($item->name == 'Aged Brie' || $item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                $item->quality = $item->quality < 50 ? $item->quality + 1 : $item->quality;
-                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->sell_in < 11 && $item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                    if ($item->sell_in < 6 && $item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            } else if ($item->quality > 0 && $item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->quality = $item->quality - 1;
-            }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
-
-            if ($item->sell_in >= 0) {
-                continue;
-            }
-
-            if (!in_array($item->name, ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert'])) {
-                $item->quality = $item->name != 'Sulfuras, Hand of Ragnaros' && $item->quality > 0
-                    ? $item->quality = $item->quality - 1
-                    : $item->quality;
-            }
-
-            if ($item->name == 'Aged Brie' && $item->quality < 50) {
-                $item->quality = $item->quality + 1;
-            }
-
-            if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                $item->quality = $item->quality - $item->quality;
+            switch ($item->name) {
+                case 'Aged Brie':
+                    $this->agedBrieUpdateQuality($item);
+                    break;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    $this->backstagePassesUpdateQuality($item);
+                    break;
+                case 'Sulfuras, Hand of Ragnaros':
+                    $this->sulfurasUpdateQuality($item);
+                    break;
+                default:
+                    $this->otherUpdateQuality($item);
             }
         }
+    }
+
+    private function agedBrieUpdateQuality(Item $item)
+    {
+        $item->quality = $item->quality < 50 ? $item->quality + 1 : $item->quality;
+        $item->sell_in -= 1;
+        $item->quality = $item->quality < 50 && $item->sell_in < 0 ? $item->quality + 1 : $item->quality;
+
+        return $item;
+    }
+
+    private function backstagePassesUpdateQuality(Item $item)
+    {
+        $item->quality = $item->quality < 50 ? $item->quality + 1 : $item->quality;
+        $item->quality = $item->quality < 50 && $item->sell_in < 11 ? $item->quality + 1 : $item->quality;
+        $item->quality = $item->quality < 50 && $item->sell_in < 6 ? $item->quality + 1 : $item->quality;
+        $item->sell_in -= 1;
+        $item->quality = $item->sell_in < 0 ? $item->quality - $item->quality : $item->quality;
+
+        return $item;
+    }
+
+    private function sulfurasUpdateQuality(Item $item)
+    {
+        return $item;
+    }
+
+    private function otherUpdateQuality(Item $item)
+    {
+        $item->quality = $item->quality > 0 ? $item->quality - 1 : $item->quality;
+        $item->sell_in -= 1;
+        $item->quality = $item->sell_in < 0 &&  $item->quality > 0 ? $item->quality - 1 : $item->quality;
+
+        return $item;
     }
 }
